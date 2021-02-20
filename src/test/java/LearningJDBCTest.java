e74a706bd7d722540cf28aa59cf1501518947f71
daulet2030 <daulet2030@gmail.com>
2021-02-20 07:46:57 +0600

    using concatenation

--- src/test/java/LearningJDBCTest.java
+++ src/test/java/LearningJDBCTest.java
@@ -8,6 +8,7 @@ import java.sql.*;
 public class LearningJDBCTest {
 
     private Connection connection;
+    private String jdbc;
 
     @BeforeClass
     public void connection() throws SQLException {
@@ -15,6 +16,7 @@ public class LearningJDBCTest {
         String user = "technostudy";
         String password = "zhTPis0l9#$&";
         connection = DriverManager.getConnection(url, user, password);
+        jdbc = "testString";
     }
 
     @Test
@@ -42,14 +44,14 @@ public class LearningJDBCTest {
     @Test
     public void insertUsers() throws SQLException {
         Statement statement = connection.createStatement();
-        int rowsAffected = statement.executeUpdate("insert into users(username, fullName) values ('jdbc', 'Java DataBase Connectivity')");
+        int rowsAffected = statement.executeUpdate("insert into users(username, fullName) values ('" + jdbc + "', 'Java DataBase Connectivity')");
         System.out.println(rowsAffected + " rowsAffected");
     }
 
     @Test
     public void deleteUsers() throws SQLException {
         Statement statement = connection.createStatement();
-        int rowsAffected = statement.executeUpdate("delete from users where username = 'jdbc'");
+        int rowsAffected = statement.executeUpdate("delete from users where username = '" + jdbc + "'");
         System.out.println(rowsAffected + " rowsAffected");
     }
 
