d9819040d5e01af9e157c33d795d876c465e230f
daulet2030 <daulet2030@gmail.com>
2021-02-20 06:35:18 +0600

    doing simple sql query

--- src/test/java/LearningJDBCTest.java
+++ src/test/java/LearningJDBCTest.java
@@ -1,2 +1,31 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
 public class LearningJDBCTest {
+
+    private Connection connection;
+
+    @BeforeTest
+    public void connection() throws SQLException {
+        String url = "jdbc:mysql://test.medis.mersys.io:33306/classicmodels";
+        String user = "technostudy";
+        String password = "zhTPis0l9#$&";
+        connection = DriverManager.getConnection(url, user, password);
+    }
+
+    @Test
+    public void gettingCustomers() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select customerName, phone, city from customers;");
+
+        while(resultSet.next()) {
+            String customerName = resultSet.getString("customerName");
+            String phone = resultSet.getString("phone");
+            String city = resultSet.getString("city");
+
+            System.out.println(customerName + "\t" + phone + "\t" + city);
+        }
+    }
 }
