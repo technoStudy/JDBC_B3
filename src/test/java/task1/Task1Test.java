051a4a6ca2e7356f955cdbb3d174287969d751f5
daulet2030 <daulet2030@gmail.com>
2021-02-23 07:46:20 +0600

    task6 solution v1 and v2

--- src/test/java/task1/Task1Test.java
+++ src/test/java/task1/Task1Test.java
@@ -5,7 +5,10 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
 
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -124,6 +127,58 @@ public class Task1Test {
         System.out.println(actualRegionId + " " + actualNumberOfCountries);
     }
 
+    @Test
+    public void task6v1() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY, MIN_SALARY, MAX_SALARY from employees join jobs on employees.JOB_ID = jobs.JOB_ID;");
+        SoftAssert softAssert = new SoftAssert();
+        while(resultSet.next()){
+            BigDecimal salary = resultSet.getBigDecimal("SALARY");
+            BigDecimal minSalary = resultSet.getBigDecimal("MIN_SALARY");
+            BigDecimal maxSalary = resultSet.getBigDecimal("MAX_SALARY");
+            softAssert.assertTrue(salary.compareTo(minSalary) > 0, getErrorMessage(resultSet, "lower", "min"));
+            softAssert.assertTrue(salary.compareTo(maxSalary) < 0, getErrorMessage(resultSet, "larger", "max"));
+        }
+
+        softAssert.assertAll();
+    }
+
+    private String getErrorMessage(ResultSet resultSet, String s, String type) throws SQLException {
+        return "Salary is " + s + " the "+type+" salary for employee ID: " + resultSet.getString("EMPLOYEE_ID") +" name: " + resultSet.getString("FIRST_NAME")+ " "+  resultSet.getString("LAST_NAME");
+    }
+
+    @DataProvider
+    public Object[][] salaryData() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY, MIN_SALARY, MAX_SALARY from employees join jobs on employees.JOB_ID = jobs.JOB_ID;");
+        resultSet.last();
+        int numberOfRows = resultSet.getRow();
+        Object[][] salaryData = new Object[numberOfRows][5];
+
+        resultSet.beforeFirst();
+
+        int index = 0;
+        while(resultSet.next()){
+            BigDecimal salary = resultSet.getBigDecimal("SALARY");
+            BigDecimal minSalary = resultSet.getBigDecimal("MIN_SALARY");
+            BigDecimal maxSalary = resultSet.getBigDecimal("MAX_SALARY");
+            String id = resultSet.getString("EMPLOYEE_ID");
+            String name = resultSet.getString("FIRST_NAME") + " " + resultSet.getString("LAST_NAME");
+            salaryData[index][0] = salary;
+            salaryData[index][1] = minSalary;
+            salaryData[index][2] = maxSalary;
+            salaryData[index][3] = id;
+            salaryData[index][4] = name;
+            index++;
+        }
+        return salaryData;
+    }
+
+    @Test(dataProvider = "salaryData")
+    public void task6v2(BigDecimal salary, BigDecimal minSalary, BigDecimal maxSalary, String id, String name){
+        Assert.assertTrue(salary.compareTo(minSalary) > 0, "Employee id " + id + " has lower salary. Name: " + name);
+        Assert.assertTrue(salary.compareTo(maxSalary) < 0, "Employee id " + id + " has larger salary. Name: " + name);
+    }
 
     @AfterClass
     public void closeConnection() throws SQLException {
